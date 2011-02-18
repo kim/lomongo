df97585d473ebd140bc4b08889587c5bd351ac25
Kim Altintop <kim.altintop@gmail.com>
2011-02-18 21:55:38 +0100

    Delineate example.
    - Connected should really be a Monad
    - funny scalaz doesn't seem to have bracket

--- src/main/scala/lol/main.scala
+++ src/main/scala/lol/main.scala
@@ -10,35 +10,46 @@ object Main {
   import lol.Bson._
   import lol.Bson.Implicits._
 
-  def main(args: Array[String]) {
-    connect("localhost", 27017) >>= (implicit conn => {
-      val db   = Database("foo")
-      val coll = Collection("bar", db)
-      val docs = Seq(
-             ("a" := "A")
-          :: ("b" := "B")
-        ,    ("a" := "A")
-          :: ("c" := List[Value]("A", "B", "C"))
-          :: ("d" := ("D" := "D")
-                  :: ("d" := "d"))
-      )
+  def example(conn: Connected) {
+    val db   = Database("foo")
+    val coll = Collection("bar", db)
+    val docs = Seq(
+         ("a" := "A")
+        :: ("b" := "B")
+      ,    ("a" := "A")
+        :: ("c" := List[Value]("A", "B", "C"))
+        :: ("d" := ("D" := "D")
+                :: ("d" := "d"))
+    )
 
-      val query: Document = ("a" := "A") :: Nil
+    val query: Document = ("a" := "A") :: Nil
 
-      val i = insert(docs)(coll)_
-      val q = find(query)(coll)_
-      val d = removeAll(query)(coll)_
-      val e = getLastError()(db)_
+    val i = insert(docs)(coll)_
+    val q = find(query)(coll)_
+    val d = removeAll(query)(coll)_
+    val e = getLastError()(db)_
 
+    def exec(implicit c: Connected) {
       run(i)
       run(e) map (println)
       run(q) map (println)
       run(d)
       assume (run(q).docs.size == 0)
+    }
+    exec(conn)
+  }
 
-      disconnect(conn)
-      none
-    })
+  def main(args: Array[String]) {
+    bracket(
+      { connect("localhost", 27017).get },
+      disconnect,
+      example
+    )
+  }
+
+  def bracket[A,B,C](before: => A, after: A => B, thing: A => C): C = {
+    val a = before
+    try { thing(a) } finally { after(a) }
   }
 }
 
